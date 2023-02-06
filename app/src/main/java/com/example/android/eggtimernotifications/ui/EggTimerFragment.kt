ada3c3a54c8729d79613f8f105b9095075cb46cb
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-02-06 15:32:02 -0500

    Adding code from section "4.2.5. (3:24) Exercise: Notification Channels"

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -24,46 +24,64 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.eggtimernotifications.R
 import com.example.android.eggtimernotifications.databinding.FragmentEggTimerBinding
-import com.google.firebase.messaging.FirebaseMessaging
 
 class EggTimerFragment : Fragment() {
 
-    private val TOPIC = "breakfast"
+	private val TOPIC = "breakfast"
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		val binding: FragmentEggTimerBinding = DataBindingUtil.inflate(
+			inflater, R.layout.fragment_egg_timer, container, false
+		)
 
-        val binding: FragmentEggTimerBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_egg_timer, container, false
-        )
+		val viewModel = ViewModelProvider(this).get(EggTimerViewModel::class.java)
 
-        val viewModel = ViewModelProvider(this).get(EggTimerViewModel::class.java)
+		binding.eggTimerViewModel = viewModel
+		binding.lifecycleOwner = this.viewLifecycleOwner
 
-        binding.eggTimerViewModel = viewModel
-        binding.lifecycleOwner = this.viewLifecycleOwner
+		// TODO: Step 1.7 call create channel
+		createChannel(
+			getString(R.string.egg_notification_channel_id),
+			getString(R.string.egg_notification_channel_name)
+		)
 
-        // TODO: Step 1.7 call create channel
+		return binding.root
+	}
 
-        return binding.root
-    }
+	private fun createChannel(channelId: String, channelName: String) {
+		// TODO: Step 1.6 START create a channel
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			val notificationChannel = NotificationChannel(
+				channelId,
+				channelName,
+				// TODO: Step 2.4 change importance
+				NotificationManager.IMPORTANCE_LOW
+			)
+			// TODO: Step 2.6 disable badges for this channel
 
-    private fun createChannel(channelId: String, channelName: String) {
-        // TODO: Step 1.6 START create a channel
+			notificationChannel.enableLights(true)
+			notificationChannel.lightColor = Color.RED
+			notificationChannel.enableVibration(true)
+			notificationChannel.description = "Time for breakfast"
 
-        // TODO: Step 1.6 END create a channel
+			val notificationManager = requireActivity().getSystemService(
+				NotificationManager::class.java
+			)
+			notificationManager.createNotificationChannel(notificationChannel)
 
-    }
+			// TODO: Step 1.6 END create a channel
+		}
+	}
 
-    companion object {
-        fun newInstance() = EggTimerFragment()
-    }
+	companion object {
+		fun newInstance() = EggTimerFragment()
+	}
 }
-
