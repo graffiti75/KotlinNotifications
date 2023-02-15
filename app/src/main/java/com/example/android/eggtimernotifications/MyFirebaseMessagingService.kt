3d4fc9c761c235a70406aecbc253fde02467a63b
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-02-15 00:08:41 -0500

    Adding code from section "4.2.24. (2:19) Exercise: Data Messages"

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -37,17 +37,17 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 	// [START receive_message]
 	override fun onMessageReceived(remoteMessage: RemoteMessage) {
 		// Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-		Log.d(TAG, "From: ${remoteMessage?.from}")
+		Log.d(TAG, "From: ${remoteMessage.from}")
 
 		// TODO Step 3.5 check messages for data
 		// Check if message contains a data payload.
-		remoteMessage?.data?.let {
+		remoteMessage.data.let {
 			Log.d(TAG, "Message data payload: " + remoteMessage.data)
 		}
 
 		// TODO Step 3.6 check messages for notification and call sendNotification
 		// Check if message contains a notification payload.
-		remoteMessage?.notification?.let {
+		remoteMessage.notification?.let {
 			Log.d(TAG, "Message Notification Body: ${it.body}")
 			sendNotification(it.body!!)
 		}
